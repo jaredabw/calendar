f0fab9d517d5bba830474f1f25c475ce13e48338
jaredabw <jwig0007@student.monash.edu>
2024-07-29 21:54:28 +1000

    make fastapi

--- cal.py
+++ cal.py
@@ -1,26 +1,46 @@
 import urllib.request
 import tempfile
+from fastapi import FastAPI, Response
 
-url = "https://my-timetable.monash.edu/even/rest/calendar/ical/50f96f6b-7019-45ab-accc-1d81eb2814b9"
-
-output = "output.ics"
+app = FastAPI()
 
 def rename_events(url):
     tmp = tempfile.NamedTemporaryFile().name
-    urllib.request.urlretrieve(url, tmp)
+    try:
+        urllib.request.urlretrieve(url, tmp)
+    except ValueError:
+        return None
     with open(tmp, "r") as f:
         lines = f.readlines()
 
+    if len(lines) == 0:
+        return None
+
     for i, line in enumerate(lines):
+        if i == 1 and "Allocate" not in line:
+            return None
+
         if line.startswith("SUMMARY:"):
             classname, classtype = line.split("\\, ")
-            classtype = classtype.rstrip().removesuffix("-JTU") + "\n"
-            classname = classname.rstrip().removeprefix("SUMMARY:") + "\n"
+            classtype = classtype.rstrip().removesuffix("-JTU")
+            classname = classname.rstrip().removeprefix("SUMMARY:")
 
             classcode = lines[i+2].removeprefix("DESCRIPTION:").split("_")[0]
             lines[i] = f"SUMMARY:{classcode} - {classtype}"
 
-    with open(output, "w") as f:
-        f.writelines(lines)
+    return "".join(lines)
 
-rename_events(url)
+@app.get("/")
+def read_root():
+    return {"message": "Hello World!"}
+
+@app.get("/{init_url:path}")
+def read_item(init_url = None):
+    content = rename_events(init_url)
+    if content is None:
+        return Response(content="File not found", media_type="text/plain")
+
+    new_filename = "modified.ics"
+    response = Response(content=content, media_type="text/calendar")
+    response.headers["Content-Disposition"] = f"attachment; filename={new_filename}"
+    return response
